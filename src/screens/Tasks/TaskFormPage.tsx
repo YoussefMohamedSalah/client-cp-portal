80612d60cefe2b0e0d370e2ce9295c5fc905eb9f
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2024-02-14 14:03:16 +0200

    init task page

---
+++ src/screens/Tasks/TaskFormPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+    id: string | null;
+}
+const TaskFormPage = ({ id }: Props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TaskFormPage
