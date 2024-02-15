f93cf67e03ba9755c67b5b8be6402016baccc46d
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2024-02-15 11:09:58 +0200

    Profile Place holder

---
+++ src/screens/Profile/EmployeeProfileDetails.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// this will contain the employee profile page 
+// to be showing for any other employee.
+
+// ANY MUTUAL COMPONENTS WOULD BE AT COMPONENTS/ACCOUNT
+// mutual components will be shared with deferent Props
+
+const EmployeeProfileDetails = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EmployeeProfileDetails
